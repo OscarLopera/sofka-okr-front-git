fa295f5aa2f4d19a887de0a060672aad28f36acc
andrescaro189 <andrescaro189@gmail.com>
2021-04-27 10:17:28 -0500

    Se ingresan las dependencias para los test y se unifica el mensaje de welcome en la pagina de sing in

--- src/pages/SingIn.js
+++ src/pages/SingIn.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Welcome from '../components/notifications/welcome/Welcome'
 
 export default function SingIn() {
     return (
         <div className='singin'>
             <h1>SingIn</h1>
+            <Welcome/>
         </div>
     )
 }
