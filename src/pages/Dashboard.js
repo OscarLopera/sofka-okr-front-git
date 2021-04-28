a29a1421f137542a96510ecb52eadede13b944ee
Sebastián Pulgarín <pulgaringomezsebastian@gmail.com>
2021-04-27 21:01:42 -0500

    Creacion de rama

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,7 @@ export default function Dashboard() {
     return (
         <div className='home'>
             <h1>Bienvenido a tu Dashboard {auth.currentUser.displayName} </h1>
+            <h3>{auth.currentUser.uid}</h3>
         </div>
     )
 }
