53871c45e45e179f2d0d538cf65af2396eb9049f
Daniela0209 <grajalesdaniela253@gmail.com>
2021-04-28 16:23:54 -0500

    add styles bell

--- src/components/ModalBell.js
+++ src/components/ModalBell.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Modal} from '@material-ui/core';
-
+import "../assets/styles/notifications/ModalBell.scss";
 import bell from "./administration/img/notifications.svg";
 import bell2 from "./administration/img/notifications_active.svg";
 
