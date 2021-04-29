39eee68b72ea70d15894289681449c769c129097
Sebastián Pulgarín <pulgaringomezsebastian@gmail.com>
2021-04-29 18:34:18 -0500

    Se agregaron pruebas, sobre que el container no este vacio, que el id no este vacio

--- src/__tests__/Dashboard.test.js
+++ src/__tests__/Dashboard.test.js
@@ -1,11 +1,23 @@
 import React from "react";
 import dashboard from "../pages/Dashboard"
 
-describe('verification id people',() =>{
-  test('verfivation id is not null', () =>{
-    expect(dashboard).not.toBeNull()
-  })
-  test('id to be definided', () =>{
-    expect(dashboard).toBeDefined()
-  })
-})
+let container = null;
+beforeEach(() => {
+  // configurar un elemento del DOM como objetivo del renderizado
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // limpieza al salir
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renderiza", () => {
+  act(() => {
+    render(<dashboard />, container);
+  });
+  expect(container.textContent).toBe("");
+});
