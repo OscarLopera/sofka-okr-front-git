87b1fa264a56bdea8fcf5b309a0954d8357c4482
jjaramga <juanfelipejg@gmail.com>
2021-04-30 00:39:54 -0500

    merge con cambios de stephanie

--- src/environment/backendurl.js
+++ src/environment/backendurl.js
@@ -1,5 +1,5 @@
 export const environment = {
 	apiOkrUrl: 'https://tranquil-island-91872.herokuapp.com/Okrs',
 	apiKrUrl: 'https://tranquil-island-91872.herokuapp.com/Krs',
-	apiRegisterUser: 'https://tranquil-island-91872.herokuapp.com/api/users'
+	apiRegisterUser: 'https://tranquil-island-91872.herokuapp.com/api/users'	
 }
