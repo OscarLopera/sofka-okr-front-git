ccaf51b1319462fbfc4e1cde9f37fbf1a8ad362f
Sebastián Pulgarín <pulgaringomezsebastian@gmail.com>
2021-04-30 11:27:18 -0500

    Se corrigio ortografia de modulo de notificaciones

--- src/components/structure/Navbar.js
+++ src/components/structure/Navbar.js
@@ -15,42 +15,42 @@ function Navbar() {
     data: [
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3a',
-            name: 'Notification Proyect',
+            name: 'Notification del Proyecto',
             typeN: 'OKR',
             detail: '30',
             viewed: 'false'
         },
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3b',
-            name: 'Notification Proyect',
+            name: 'Notification del Proyecto',
             typeN: 'OKR',
             detail: '70',
             viewed: 'true'
         },
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3b',
-            name: 'Notification Proyect',
+            name: 'Notification del Proyecto',
             typeN: 'OKR',
             detail: '100 Or Finished',
             viewed: 'true'
         },
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3a',
-            name: 'Notification Proyect',
+            name: 'Notification del Proyecto',
             typeN: 'KR',
             detail: '30',
             viewed: 'true'
         },
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3b',
-            name: 'Notification Proyect',
+            name: 'Notification del Proyecto',
             typeN: 'KR',
             detail: '70',
             viewed: 'true'
         },
         {
             id: 'd00d3614-101a-44ca-b6c2-0be075aeed3b',
-            name: 'Notification Proyect ',
+            name: 'Notification del Proyecto ',
             typeN: 'KR',
             detail: '100',
             viewed: 'true'
